c61fb4c34119bfcd5c197758925e8485057ad558
monahvi <nahvi.moghaddam@gmail.com>
2023-09-16 20:23:04 +0100

    feature start

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import Featured from "../../components/featured/Featured";
+import "./home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </div>
   );
 };

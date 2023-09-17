e57ee7b60411dddbfb2f6076d4fb472a0dd37113
monahvi <nahvi.moghaddam@gmail.com>
2023-09-17 09:31:49 +0100

    property list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import Featured from "../../components/featured/Featured";
 import "./home.css";
+import PropertyList from "../../components/propertyList/PropertyList";
 
 const Home = () => {
   return (
@@ -11,6 +12,8 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
       </div>
     </div>
   );
